77cb379a95cb2e1bddd455558d2ea5f37c572cb9
Jorgeeerrl <jorgeeerrl@gmail.com>
2024-06-10 10:18:10 +0200

    TC23 + TC24 añadidos

--- conftest.py
+++ conftest.py
@@ -14,6 +14,10 @@ def driver(request):
     options.add_argument("--start-maximized")
     options.add_argument('--window-size=1920,1080')
 
+    download_folder = os.path.join(os.getcwd(), 'recursos')
+    preferences = {"download.default_directory": download_folder}  # Define una preferencia.
+    options.add_experimental_option("prefs", preferences)  # Aplica las preferencias definidas.
+
     driver = webdriver.Chrome(options=options)  # Inicializa una instancia de WebDriver con las opciones definidas.
     request.cls.driver = driver # Asigna el WebDriver a la clase de prueba, permitiendo su uso en los métodos de prueba.
     yield driver
